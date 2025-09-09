085e52fb054129733f653d0521860bce1ec34713
Cyrus Webs <103361084+CyrusWebs@users.noreply.github.com>
2025-09-09 12:32:04 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
+function sendmail() {
+    let params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        subject : document.getElementById("subject").value
+        message: document.getElementById("message").value
+    }
+
+    emailjs.send("service_9w69vh2","template_s7bbvmm",parms).then(alert("Success! Thank you for contacting me. I will get back to you as soon as possible."))
+
+
 (function ($) {
     "use strict";
 
